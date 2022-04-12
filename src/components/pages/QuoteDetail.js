262d4db9dd83907a397f1e49318a5828d902a008
JuanPaladines <juan@twnel.com>
2022-04-12 13:36:21 -0500

    Outputting Data

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -2,13 +2,25 @@ import { useParams } from "react-router-dom";
 import { Fragment } from "react";
 import { Route } from "react-router-dom";
 import Comments from "../comments/Comments";
+import HighlightedQuote from '../quotes/HighlightedQuote'
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is Fun!" },
+  { id: "q2", author: "Jhon", text: "Learning Node is Great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+  if(!quote){
+    return <p>No quote found!</p>
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path='/quotes/:quoteId/comments'>
         <Comments />
       </Route>
