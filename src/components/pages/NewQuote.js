f0352e443e087d82762b7e8f5efb9a8738b6587f
JuanPaladines <juan@twnel.com>
2022-04-12 14:01:52 -0500

    Programmatic Navigation

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuotes = () => {
+  const history = useHistory();
   const addQuoreHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.replace('/quotes')
   };
 
   return <QuoteForm onAddQuote={addQuoreHandler} />;
